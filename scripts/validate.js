e7cc2db1697324725acc476a80aa009e0420e219
Alina Vayer <lovering2007@mail.ru>
2021-02-06 16:20:15 +0300

    создан и подключен validate.js

---
+++ scripts/validate.js
@@ -0,0 +1,14 @@
+// функция, которая проверяет поля ввода
+// function validationPopupProfile(eventTarget, spanError) {
+//   return () => {
+//     if (eventTarget.value.length === 0) {
+//       eventTarget.setCustomValidity("Это поле обязательно");
+//     } else if (eventTarget.value.length === 1 || eventTarget.value.length > 30) {
+//       eventTarget.setCustomValidity("Не меньше 2 и не больше 30 символов");
+//     } else {
+//       eventTarget.setCustomValidity("");
+//     }
+//     spanError.textContent = eventTarget.validationMessage;
+//   }
+// }
+
