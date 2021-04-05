ebfdba3631cbc185a0c46b9580c562ed7ea618c0
Alina Vayer <lovering2007@mail.ru>
2021-04-05 10:55:43 +0300

    рефакторинг

--- src/components/Api.js
+++ src/components/Api.js
@@ -6,17 +6,17 @@ class Api {
 
     _getResponseData(res) {
         if (!res.ok) {
-            return Promise.reject(`Ошибка: ${res.status}`); 
+            return Promise.reject(`Ошибка: ${res.status}`);
         }
         return res.json();
-      } 
+    }
 
     getPersonalInfo() {
         return fetch(`${this._baseUrl}/users/me`, {
             method: 'GET',
             headers: this._headers
         })
-        .then(this._getResponseData); 
+            .then(this._getResponseData);
     }
 
     getInitialCards() {
@@ -24,7 +24,7 @@ class Api {
             method: 'GET',
             headers: this._headers
         })
-        .then(this._getResponseData); 
+            .then(this._getResponseData);
     }
 
     showUserInfo(data) {
@@ -36,7 +36,7 @@ class Api {
                 about: data.about
             })
         })
-        .then(this._getResponseData); 
+            .then(this._getResponseData);
     }
 
     addNewCard(data) {
@@ -48,7 +48,7 @@ class Api {
                 name: data.name
             })
         })
-        .then(this._getResponseData); 
+            .then(this._getResponseData);
     }
 
     showLikesNumber(cardId) {
@@ -56,7 +56,7 @@ class Api {
             method: 'PUT',
             headers: this._headers
         })
-        .then(this._getResponseData); 
+            .then(this._getResponseData);
     }
 
     deleteCard(cardId) {
@@ -64,7 +64,7 @@ class Api {
             method: 'DELETE',
             headers: this._headers
         })
-        .then(this._getResponseData); 
+            .then(this._getResponseData);
     }
 
     unlikeCard(cardId) {
@@ -72,7 +72,7 @@ class Api {
             method: 'DELETE',
             headers: this._headers
         })
-        .then(this._getResponseData); 
+            .then(this._getResponseData);
     }
 
     editAvatar(data) {
@@ -83,7 +83,7 @@ class Api {
                 avatar: data.avatar
             })
         })
-        .then(this._getResponseData); 
+            .then(this._getResponseData);
     }
 }
 
