c83b8885bae69db344c39d2a07dcee99575e70ae
Alina Vayer <lovering2007@mail.ru>
2021-04-05 10:56:46 +0300

    рефакторинг

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,23 +5,22 @@ class Section {
         this._container = document.querySelector(containerSelector);
     }
 
-//метод, который отвечает за отрисовку данных на странице
+    //метод, который отвечает за отрисовку данных на странице
     renderItems(userData) {
         this._renderedItems.forEach(item => {
             this._renderer(item, userData);
         })
     }
 
-//метод, который принимает DOM-элемент и добавляет его в контейнер
+    //методы, которые принимают DOM-элемент и добавляет его в контейнер
     addItem(element) {
         this._container.append(element);
     }
-
     addItemPrepend(element) {
         this._container.prepend(element);
     }
 
-//метод, который устанавливает отрисованные элементы
+    //метод, который устанавливает отрисованные элементы
     setRenderedItems(item) {
         this._renderedItems = item;
     }
